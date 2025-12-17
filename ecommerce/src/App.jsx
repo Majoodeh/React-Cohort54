d45cd29b8eec852692bb0bc140f28d0b0ca49917
Majoodeh <Majdhussein144@gmail.com>
2025-12-17 08:19:32 +0100

    tailwaind , blueprint of the buttons and cards

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import CategoryBtn from "../components/category";
+import allCategories from "./fake-data/all-categories";
+import Product from "../components/product";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+          <CategoryBtn title={allCategories[0]} />
+          <CategoryBtn title={allCategories[1]} />
+          <CategoryBtn title={allCategories[2]} />
+          <CategoryBtn title={allCategories[3]} />
+        </div>
+        <div>
+          <Product />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

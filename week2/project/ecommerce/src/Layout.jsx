2d1ed5766ef96c1273d3a0b39ae04a798912aa73
Majoodeh <Majdhussein144@gmail.com>
2026-01-10 20:00:13 +0100

    week2 assignment

--- week2/project/ecommerce/src/Layout.jsx
+++ week2/project/ecommerce/src/Layout.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header.jsx";
-import Home from "./components/Home.jsx";
-
-import { useState } from "react";
 import "./App.css";
-import Category from "./components/Category";
-import Product from "./components/Product";
 
 function Layout() {
   return (
     <>
       <Header />
-
       <Outlet />
     </>
   );
